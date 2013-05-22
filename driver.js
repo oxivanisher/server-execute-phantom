b5e27f5ce650ceec093fd3a51cb71437d314d6e4
Jason A. Donenfeld <Jason@zx2c4.com>
2013-05-22 13:09:55 +0200

    Proper user agent.

--- driver.js
+++ driver.js
@@ -4,7 +4,7 @@ var args = require("system").args;
 if (args.length != 2)
 	phantom.exit();
 
-page.settings.userAgent = "Server Execute Phantom";
+page.settings.userAgent = "Mozilla/5.0 (compatible; ZX2C4 Server Execute Phantom/1.0; +http://git.zx2c4.com/server-execute-phantom/about/)";
 page.open(args[1], function() {
 	console.log(page.content);
 	phantom.exit();
